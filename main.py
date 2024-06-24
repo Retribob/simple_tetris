1d13c7d32851938e7bdde44c96c43cd33e9edad3
Retribob <97873662+Retribob@users.noreply.github.com>
2024-06-24 15:44:07 +0800

    Updated to include kicks, drop speed

--- main.py
+++ main.py
@@ -8,125 +8,233 @@ from preview import Preview
 from score import Score
 from sys import exit
 from button1 import Button
+from inputBox import InputBox
+
+#things to note:
+#1. the game does not have a seven bag system. Pieces are completely random.
+#2. No swap pieces
+#To do:
+#1. Allow rotations when pieces are next to other pieces.
+#2. Some bugs with ghost pieces and rotations
+#3. Implement a settings menu to adjust sound and control settings.
+#4. Implement a timing in which you can still move the piece after it can no longer move down
 
 
 #initializes pygame
 pygame.init()
 frame_tick = pygame.time.Clock()
 
-#windows and surfaces
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-
-def create_new_tetromino():
-	tetromino = Tetromino(sprites, choice(list(TETROMINOS.keys())))
-
-
+#global function for font size
 def get_font(size):
-	return pygame.font.Font("assets/font.ttf", size)
-
-
-preview_pieces = [choice(list(TETROMINOS.keys())) for shape in range(3)]
-def new_piece():
-	new_piece = preview_pieces.pop(0)
-	preview_pieces.append(choice(list(TETROMINOS.keys())))
-	return new_piece
-
-
-score = Score()
-
-def score_update(score1, level1):
-	score.myscore = score1
-	score.mylevel = level1
+		return pygame.font.Font("assets/font.ttf", size)
 
 
 
-#timer_event = pygame.USEREVENT+1
-#pygame.time.set_timer(timer_event, game.time_interval)
-game = Game(new_piece, score_update)
-preview = Preview()
 
 
-def tetrisGame():
-#this is where everything is run
-	while True:
-		window.fill('black')
-		score.run()
-		preview.run(preview_pieces)
-		game.run()
-
-		if game.mouseclick:
-			pause_menu()
-
-
-		pygame.display.update()
-		frame_tick.tick(60)
+class TetrisGame():
+	def __init__(self, get_font):
+		self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.get_font = get_font
+		self.preview_pieces = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+		self.game = Game(self.new_piece, self.score_update)
+		self.preview = Preview()
+		self.score = Score()
+		self.new_game = False
 
 
-def setting_menu():
-	while True:
-		window.fill('black')
-		pygame.display.update()
-		frame_tick.tick(60)
+	#this function takes a piece from the preview list and returns the piece
+	def new_piece(self):
+		self.next_piece = self.preview_pieces.pop(0)
+		self.preview_pieces.append(choice(list(TETROMINOS.keys())))
+		return self.next_piece
 
+	#function for updating score and level between score.py and tetromino.py
+	def score_update(self, score1, level1):
+		self.score.myscore = score1
+		self.score.mylevel = level1
 
-def pause_menu():
-	while True:
-		window.fill('black')
-		RESUME_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'RESUME', font=get_font(30))
-		MENU_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*14, 'MAIN MENU', font=get_font(30))
-		QUIT1_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*16, 'QUIT', font=get_font(30))
+	#game over function. Draws over the game board and gives the option to quit or retry
+	def game_over(self):
+		while True:
+			self.window.fill('black')
+			self.GAME_OVER_TEXT = get_font(50).render("GAME OVER", True, '#FFFFFF')
+			self.GAME_OVER_RECT = self.GAME_OVER_TEXT.get_rect(center = (WINDOW_WIDTH/2, CELL_SIZE*10))
+			self.RETRY_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*14, 'RETRY', font=self.get_font(30))
+			self.QUIT2_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*16, 'QUIT', font=self.get_font(30))
+			self.RETRY_BUTTON.mouseInput()
+			self.RETRY_BUTTON.run()
+			self.QUIT2_BUTTON.mouseInput()
+			self.QUIT2_BUTTON.run()
 
-		for button in [RESUME_BUTTON, MENU_BUTTON, QUIT1_BUTTON]:
-			button.mouseInput()
-			button.run()
-
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				exit()
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if RESUME_BUTTON.mouseInput():
-					tetrisGame()
-				elif MENU_BUTTON.mouseInput():
-					main_menu()
-				elif QUIT1_BUTTON.mouseInput():
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
 					pygame.quit()
 					exit()
-		pygame.display.update()
-		frame_tick.tick(60)
+				elif event.type == pygame.MOUSEBUTTONDOWN:
+					if self.RETRY_BUTTON.mouseInput():
+						self.new_game = True
+						self.game_board()
+					if self.QUIT2_BUTTON.mouseInput():
+						pygame.quit()
+						exit()
+			self.window.blit(self.GAME_OVER_TEXT, self.GAME_OVER_RECT)
+			pygame.display.update()
+			frame_tick.tick(60)
 
-def main_menu():
-	while True:
+	#pause menu function. Draws over the game board when the 'pause' button is clicked on
+	def pause_menu(self):
+		while True:
+			self.window.fill('black')
+			self.RESUME_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'RESUME', font=self.get_font(30))
+			self.MENU_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*14, 'MAIN MENU', font=self.get_font(30))
+			self.QUIT1_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*16, 'QUIT', font=self.get_font(30))
 
-		window.fill('black')
-		MENU_TEXT = get_font(50).render("TETRIS", True, "#FFFFFF")
-		MENU_RECT = MENU_TEXT.get_rect(center = (WINDOW_WIDTH/2, CELL_SIZE*4))
+			for button in [self.RESUME_BUTTON, self.MENU_BUTTON, self.QUIT1_BUTTON]:
+				button.mouseInput()
+				button.run()
 
-		PLAY_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*10, 'PLAY', font=get_font(30))
-		SETTINGS_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'SETTINGS', font=get_font(30))
-		QUIT_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*14, 'QUIT',  font=get_font(30))
-
-		for button in [PLAY_BUTTON, SETTINGS_BUTTON, QUIT_BUTTON]:
-			button.mouseInput()
-			button.run()
-
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				exit()
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if PLAY_BUTTON.mouseInput():
-					tetrisGame()
-				elif SETTINGS_BUTTON.mouseInput():
-					setting_menu()
-				elif QUIT_BUTTON.mouseInput():
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
 					pygame.quit()
 					exit()
+				elif event.type == pygame.MOUSEBUTTONDOWN:
+					if self.RESUME_BUTTON.mouseInput():
+						self.game_board()
+					elif self.MENU_BUTTON.mouseInput():
+						self.main_menu()
+					elif self.QUIT1_BUTTON.mouseInput():
+						pygame.quit()
+						exit()
+			pygame.display.update()
+			frame_tick.tick(60)
 
 
-		window.blit(MENU_TEXT, MENU_RECT)
-		pygame.display.update()
-		frame_tick.tick(60)
+	#game function. Draws the preview, score and game board. The game is run from this function.
+	def game_board(self):
+		if self.new_game:
+			self.preview_pieces = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+			self.game = Game(self.new_piece, self.score_update)
+			self.preview = Preview()
+			self.score = Score()
+			self.new_game = False
 
-main_menu()
+		pygame.mixer.music.unload()
+		pygame.mixer.music.load('music/03. A-Type Music (Korobeiniki).mp3')
+		pygame.mixer.music.set_volume(0)
+		pygame.mixer.music.play(-1)
+
+		while True:
+			self.window.fill('black')
+			self.score.run()
+			self.preview.run(self.preview_pieces)
+			self.game.run()
+
+			if self.game.mouseclick:
+				self.pause_menu()
+
+
+			if self.game.game_over:
+				self.game_over()
+
+			pygame.display.update()
+			frame_tick.tick(60)
+
+
+#settings menu. Currently unused.
+#	def settings_change(self):
+#		self.window.fill('black')
+#		self.Input_Box = InputBox(WINDOW_WIDTH/2, CELL_SIZE*9)
+#		while True:
+#			self.Input_Text = get_font(20).render('please key in your controls', True, '#FFFFFF')
+#			self.Input_Text_Rect = self.Input_Text.get_rect(center = (WINDOW_WIDTH/2, CELL_SIZE*7))
+#			for event in pygame.event.get():
+#				if event.type == pygame.QUIT:
+#					pygame.quit()
+#					exit()
+#				if event.type == pygame.KEYDOWN:
+#					print(self.Input_Box.text)
+#					self.Input_Box.key_event(event)
+#
+#			self.window.blit(self.Input_Text, self.Input_Text_Rect)
+#			self.Input_Box.run()
+#			pygame.display.update()
+#			frame_tick.tick(60)
+#
+
+#	def settings_menu(self):
+#		self.window.fill('black')
+#		while True:
+#			self.CLICK_TO_CHANGE = get_font(45).render('click to change', True, '#FFFFFF')
+#			self.CLICK_RECT = self.CLICK_TO_CHANGE.get_rect(center=(WINDOW_WIDTH/2, CELL_SIZE*7))
+#			self.MOVELEFT_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*10, 'LEFT CONTROLS', font=self.get_font(30))
+#			self.MOVERIGHT_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'RIGHT CONTROLS', font=self.get_font(30))
+#			self.ROTATE90_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*14, 'ROTATE CLOCKWISE', font=self.get_font(30))
+#			self.ROTATE_90_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*16, 'ROTATE ANTICLOCKWISE', font=self.get_font(30))
+#			self.SOFTDROP_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*18, 'SOFTDROP CONTROLS', font=self.get_font(30))
+#			self.SOUND_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*20, 'SOUND VOLUME', font=self.get_font(30))
+#			self.BACK_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*23, 'Back', font=self.get_font(30))
+#
+#
+#			for button in [self.MOVELEFT_BUTTON, self.MOVERIGHT_BUTTON, self.ROTATE90_BUTTON, self.ROTATE_90_BUTTON, self.SOFTDROP_BUTTON, self.SOUND_BUTTON, self.BACK_BUTTON]:
+#				button.mouseInput()
+#				button.run()
+#
+#			for event in pygame.event.get():
+#				if event.type == pygame.QUIT:
+#					pygame.quit()
+#					exit()
+#				elif event.type == pygame.MOUSEBUTTONDOWN:
+#					if self.BACK_BUTTON.mouseInput():
+#						self.main_menu()
+#					elif self.MOVELEFT_BUTTON.mouseInput():
+#						self.settings_change()
+#
+#			self.window.blit(self.CLICK_TO_CHANGE, self.CLICK_RECT)
+#			pygame.display.update()
+#			frame_tick.tick(60)
+
+#this function is run when main is first run. Clicking the play button starts the game and also resets the previous board state.
+	def main_menu(self):
+		pygame.mixer.music.unload()
+		pygame.mixer.music.load('music/01. Title.mp3')
+		pygame.mixer.music.set_volume(0)
+		pygame.mixer.music.play(-1)
+
+		while True:
+
+			self.window.fill('black')
+			self.MENU_TEXT = get_font(50).render("TETRIS", True, "#FFFFFF")
+			self.MENU_RECT = self.MENU_TEXT.get_rect(center = (WINDOW_WIDTH/2, CELL_SIZE*4))
+			self.PLAY_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*10, 'PLAY', font=self.get_font(30))
+			#self.SETTINGS_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'SETTINGS', font=self.get_font(30))
+			self.QUIT_BUTTON = Button(WINDOW_WIDTH/2, CELL_SIZE*12, 'QUIT',  font=self.get_font(30))
+
+
+			for button in [self.PLAY_BUTTON, self.QUIT_BUTTON]:
+				button.mouseInput()
+				button.run()
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit()
+				elif event.type == pygame.MOUSEBUTTONDOWN:
+					if self.PLAY_BUTTON.mouseInput():
+						self.new_game = True
+						self.game_board()
+					#if self.SETTINGS_BUTTON.mouseInput():
+					#	self.settings_menu()
+					if self.QUIT_BUTTON.mouseInput():
+						pygame.quit()
+						exit()
+			self.window.blit(self.MENU_TEXT, self.MENU_RECT)
+			pygame.display.update()
+			frame_tick.tick(60)
+
+
+
+
+main = TetrisGame(get_font)
+main.main_menu()
+
--- tetromino.py
+++ tetromino.py
@@ -1,11 +1,11 @@
 import pygame
-import time
+from timer import Timer
 from settings import *
 from random import choice
 from sys import exit
 from button1 import Button
 
-
+#game class. The game class runs the generation of pieces as sprites,  the generation of ghost pieces as sprites as well as movement and controls
 class Game:
 	def __init__(self, new_piece, score_update):
 		self.board_surface = pygame.Surface((BOARD_WIDTH, BOARD_HEIGHT))
@@ -19,7 +19,7 @@ class Game:
 		self.mask_surface.fill(BLACK)
 		self.font = pygame.font.Font("assets/font.ttf", 13)
 
-
+		self.game_over = False
 		self.board_data = [[0 for x in range(10)] for y in range(24)]
 		self.sprites = pygame.sprite.Group()
 		self.tetromino = Tetromino(self.sprites, choice(list(TETROMINOS.keys())), self.create_new_tetromino, self.board_data)
@@ -29,10 +29,12 @@ class Game:
 		self.new_level = 1
 		self.score_interval = score_interval
 		self.time_interval = TIME_INTERVAL
+		self.time_interval2 = 1
 
 		self.timer_event = pygame.USEREVENT+1
 		pygame.time.set_timer(self.timer_event, self.time_interval)
-
+		self.timer_event2 = pygame.USEREVENT+2
+		pygame.time.set_timer(self.timer_event2, self.time_interval2)
 
 	#draw grid function
 	def grid(self):
@@ -44,19 +46,19 @@ class Game:
 			y = i*CELL_SIZE
 			pygame.draw.line(self.grid_surface, LINE_COLOUR, (0, y), (self.grid_surface.get_width(), y))
 
+	#check pieces pos on the board, if y pos is below a certain value, run game over function in main
 	def check_game_over(self):
 		for cell in self.tetromino.cells:
 			if cell.pos.y < 3:
-				exit()
+				self.game_over = True
 
-
-
+	#creates a new tetromino. also checks for row clear and game over whenever a new piece is generated.
 	def create_new_tetromino(self):
 		self.clear_rows()
 		self.check_game_over()
 		self.tetromino = Tetromino(self.sprites, self.new_piece(), self.create_new_tetromino, self.board_data)
 
-
+	#very simple and score counter. Every row cleared is 100 score. Once score exceeds an interval drop speed is increased.
 	def score_multiplier(self, delete_rows):
 		self.new_score += 100 * delete_rows
 
@@ -69,12 +71,12 @@ class Game:
 
 		self.score_update(self.new_score, self.new_level)
 
-
+	#function for increasing drop speed once the arrow down key is pressed
 	def decrease_time(self, direction):
-		self.time_interval -= direction*100
+		self.time_interval -= direction*200
 		pygame.time.set_timer(self.timer_event, self.time_interval)
 
-
+	#clear row function. Check board data for a full row and clears both the board data and the sprites contained in the board data
 	def clear_rows(self):
 		delete_rows = []
 		for i, row in enumerate(self.board_data):
@@ -96,7 +98,7 @@ class Game:
 			for cell in self.sprites:
 				self.board_data[int(cell.pos.y)][int(cell.pos.x)] = cell
 
-
+	#run function that is called in the main loop
 	def run(self):
 		self.PAUSE_BUTTON = Button(CELL_SIZE, CELL_SIZE, '||',self.font)
 		self.mouseclick = False
@@ -107,18 +109,24 @@ class Game:
 				exit()
 			elif event.type == self.timer_event:
 				self.tetromino.move_down(self.sprites)
+			elif event.type == self.timer_event2:
+				self.tetromino.move_down_ghostcell(self.sprites)
 			elif event.type == pygame.MOUSEBUTTONDOWN:
 				if self.PAUSE_BUTTON.mouseInput():
 					self.mouseclick = True
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
+					self.tetromino.move_leftright_ghostcell(-1, self.sprites)
 					self.tetromino.move_leftright(-1, self.sprites)
 				if event.key == pygame.K_RIGHT:
+					self.tetromino.move_leftright_ghostcell(1, self.sprites)
 					self.tetromino.move_leftright(1, self.sprites)
 				if event.key == pygame.K_x:
 					self.tetromino.rotate(self.sprites, 1)
+					self.tetromino.rotate_ghostcell(self.sprites, 1)
 				if event.key == pygame.K_z:
 					self.tetromino.rotate(self.sprites, -1)
+					self.tetromino.rotate_ghostcell(self.sprites, -1)
 				if event.key == pygame.K_DOWN:
 					self.decrease_time(1)
 			elif event.type == pygame.KEYUP:
@@ -129,29 +137,45 @@ class Game:
 	        #blit group
 		self.PAUSE_BUTTON.mouseInput()
 		self.PAUSE_BUTTON.run()
-		self.window.blit
 		self.window.blit(self.board_surface, (6*CELL_SIZE, 2*CELL_SIZE))
 		self.window.blit(self.grid_surface, (6*CELL_SIZE, 2*CELL_SIZE))
 		self.window.blit(self.mask_surface, (6*CELL_SIZE, 2*CELL_SIZE))
 
+		self.tetromino.check_overlap()
 		self.grid()
 		self.sprites.update()
 		self.board_surface.fill(GRAY)
 		self.sprites.draw(self.board_surface)
-
-
 		self.clear_rows()
+
+
+
 
 class Tetromino:
 
 	def __init__(self, group, shape, create_new_tetromino, board_data):
 		self.shape = shape
 		self.blockpos = TETROMINOS[shape]['shape']
-		self.color = TETROMINOS[shape]['color'] 
+		self.color = TETROMINOS[shape]['color']
 		self.cells = [Cell(group, pos, self.color) for pos in self.blockpos]
+		self.ghostcells = [Ghostcell(group, pos) for pos in self.blockpos]
 		self.create_new_tetromino = create_new_tetromino
 		self.board_data = board_data
 
+
+	def check_overlap(self):
+		self.ghostpos = []
+		self.cellpos = []
+		for cell in self.cells:
+			self.cellpos.append(cell.pos.x)
+			self.cellpos.append(cell.pos.y)
+		for cell in self.ghostcells:
+			self.ghostpos.append(cell.pos.x)
+			self.ghostpos.append(cell.pos.y)
+
+		if self.ghostpos == self.cellpos:
+			for sprite in self.ghostcells:
+				sprite.kill()
 
 	def move_down(self, group):
 		if not self.vertical_collide(self.cells):
@@ -160,8 +184,8 @@ class Tetromino:
 		else:
 			for cell in self.cells:
 				self.board_data[int(cell.pos.y)][int(cell.pos.x)] = cell
-
 			self.create_new_tetromino()
+
 
 
 	def move_leftright(self, move, group):
@@ -169,19 +193,21 @@ class Tetromino:
 			for cell in self.cells:
 				cell.pos.x += move
 
-
 	def rotate(self, group, direction):
 		if self.shape != 'O':
 			pivot_pos = self.cells[0].pos
 			rotated = [cell.rotate(pivot_pos, direction) for cell in self.cells]
-
 			for pos in rotated:
-				#horizontal
-				if not 0 <= pos.x < 10:
-					return
+				if not 0 <= pos.x:
+					for pos in rotated:
+						pos.x += 1
+				if not pos.x < 10:
+					for pos in rotated:
+						pos.x -= 1
 
 				if not pos.y < 24:
-					return
+					for pos in rotated:
+						pos.y -= 1
 
 				if self.board_data[int(pos.y)][int(pos.x)]:
 					return
@@ -204,6 +230,67 @@ class Tetromino:
 		else:
 			return False
 
+#ghostcell section
+
+	def move_down_ghostcell(self, group):
+		if not self.vertical_collide_ghostcell(self.ghostcells):
+			for cell in self.ghostcells:
+				cell.pos.y += 1
+
+
+	def move_leftright_ghostcell(self, move, group):
+		if not self.horizontal_collide_ghostcell(self.ghostcells, move) and not self.horizontal_collide(self.cells, move):
+			for cell in self.ghostcells:
+				cell.pos.x += move
+			if self.piece_collide_ghostcell(self.ghostcells) and not self.horizontal_collide(self.cells, move):
+				while self.piece_collide_ghostcell(self.ghostcells):
+					for cell in self.ghostcells:
+						cell.pos.y -= 1
+
+
+	def vertical_collide_ghostcell(self, ghostcells):
+		collision_list = [cell.vertical_collide(int(cell.pos.y + 1), self.board_data) for cell in self.ghostcells]
+		if any(collision_list) == True:
+			return True
+		else:
+			return False
+
+
+	def horizontal_collide_ghostcell(self, ghostcells, move):
+		collision_list1 = [cell.ghost_collide1(int(cell.pos.x + move)) for cell in self.ghostcells]
+		if any(collision_list1) == True:
+			return True
+
+
+	def piece_collide_ghostcell(self, ghostcells):
+		collision_list = [cell.ghost_collide2(int(cell.pos.x), self.board_data) for cell in self.ghostcells]
+		if any(collision_list) == True:
+			return True
+
+	def rotate_ghostcell(self, group, direction):
+		if self.shape != 'O':
+			pivot_pos = self.ghostcells[0].pos
+			rotated = [cell.rotate(pivot_pos, direction) for cell in self.ghostcells]
+			for pos in rotated:
+				if not 0 <= pos.x:
+					for pos in rotated:
+						pos.x += 1
+				if not pos.x < 10:
+					for pos in rotated:
+						pos.x -= 1
+
+				if not pos.y < 24:
+					for pos in rotated:
+						pos.y -= 1
+
+				if self.board_data[int(pos.y)][int(pos.x)]:
+					for pos in rotated:
+						pos.y -= 1
+
+			for i, cell in enumerate(self.ghostcells):
+				cell.pos = rotated[i]
+
+
 class Cell(pygame.sprite.Sprite):
 
 	def __init__(self, group, pos, color):
@@ -212,6 +299,8 @@ class Cell(pygame.sprite.Sprite):
 		self.image = pygame.image.load(f'assets/{color}.png').convert_alpha()
 		self.pos = pygame.Vector2(pos) + (4, 3)
 		self.rect = self.image.get_rect(topleft = self.pos * CELL_SIZE)
+		self.cell_mask = pygame.mask.from_surface(self.image)
+
 
 	def rotate(self, pivot_pos, direction):
 		distance = self.pos - pivot_pos
@@ -222,6 +311,43 @@ class Cell(pygame.sprite.Sprite):
 	def horizontal_collide(self, posx, board_data):
 		if not 0 <= posx < 10:
 			return True
+		if board_data[int(self.pos.y)][posx]:
+			return True
+
+	def vertical_collide(self, posy, board_data):
+		if posy >= 24:
+			return True
+		if board_data[posy][int(self.pos.x)]:
+			return True
+
+	def update(self):
+		self.rect.topleft = self.pos * CELL_SIZE
+
+
+
+class Ghostcell(pygame.sprite.Sprite):
+	def __init__(self, group, pos):
+		super().__init__(group)
+		self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+		self.image.fill('#FFFFFF')
+		self.image.set_alpha(50)
+		self.pos = pygame.Vector2(pos) + (4, 3)
+		self.rect = self.image.get_rect(topleft = self.pos * CELL_SIZE)
+
+
+
+	def rotate(self, pivot_pos, direction):
+		distance = self.pos - pivot_pos
+		rotated_vector = distance.rotate(direction*90)
+		new_pos = pivot_pos + rotated_vector
+		return new_pos
+
+
+	def ghost_collide1(self, posx):
+		if not 0 <= posx < 10:
+			return True
+
+	def ghost_collide2(self, posx, board_data):
 		if board_data[int(self.pos.y)][posx]:
 			return True
 
